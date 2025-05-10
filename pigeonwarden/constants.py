3096e6556280400723f88b978aa105c6d3528295
tomasvana10 <tomasvana@yahoo.com>
2025-05-10 22:24:27 +1000

    fix BASE_PATH parents

--- pigeonwarden/constants.py
+++ pigeonwarden/constants.py
@@ -2,7 +2,7 @@ from pathlib import Path
 
 DATASET_URL = "https://app.roboflow.com/ds/nZXCefQ1AU?key=YgRwnTbunC"
 
-BASE_PATH = Path(__file__).resolve().parents[2]
+BASE_PATH = Path(__file__).resolve().parents[1]
 DATASET_PATH = BASE_PATH / "datasets" / "pigeons"
 RUNS_PATH = BASE_PATH / "runs"
 DETECT_PATH = RUNS_PATH / "detect"
