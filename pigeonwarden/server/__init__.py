461e7747cb78d03651a1a7e7df2a087fc1585380
tomasvana10 <tomasvana@yahoo.com>
2025-05-12 19:08:08 +1000

    init_server default arg

--- pigeonwarden/server/__init__.py
+++ pigeonwarden/server/__init__.py
@@ -4,6 +4,7 @@ from .. import get_available_port, is_port_in_use
 from ..warden import Warden
 from .routes import init_routes
 
+
 PORT = 6969
 HOST = "0.0.0.0"
 CONFIG = "config.json"
@@ -12,7 +13,9 @@ app = Flask(__name__)
 app.config["TEMPLATES_AUTO_RELOAD"] = True
 
 
-def init_server(*, host: str, port: str) -> None:
+def init_server(*, host: str = HOST, port: int = PORT) -> None:
+    # If you want to make pigeonwarden work with anything other than the Flask development
+    # server PLEASE be my guest and submit a pull request because I have given up
     warden = Warden(fps=4)
     init_routes(app, warden, CONFIG)
     warden.start_inference()
