f3a53e1111f2df9e0446f44990a94b07242bbfd3
tomasvana10 <124552709+tomasvana10@users.noreply.github.com>
2025-09-22 17:16:42 +1000

    Update add_secret.py

--- scripts/add_secret.py
+++ scripts/add_secret.py
@@ -1,9 +1,10 @@
 import secrets
+import sys
 
 from dotenv import set_key
 
 _env = ".env.local"
-_key = "SECRET_KEY"
+_key = str(sys.argv[1])
 
 secret = secrets.token_hex(32)
 set_key(_env, _key, secret)
